b5303a397b59e99554ddbe69da4da498cad41278
Maximo Guzman <vmaximoguzman@gmail.com>
2022-10-13 18:01:43 -0300

    HW 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,28 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let dec = 0;
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < num.length; i++) {
+      dec += num[i] * (Math.pow(2, (num.length - 1 - i)));
+   }
+   return dec;
+}
+
+function DecimalABinario(num) {
+   let div = num;
+   let rest = "";
+
+   while (div !== 0) {
+      let restDiv = div%2;
+      div = Math.floor(div/2);
+      rest = restDiv + rest;
+   }
+
+   return rest;
+
+   //return num.toString(2);
+}
 
 module.exports = {
    BinarioADecimal,
